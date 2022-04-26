3946540bfbb99f016fd33ff6adc163768ae523a2
geert verspaij <geertverspaij@hotmail.nl>
2022-04-26 11:18:09 +0200

    More on components -Interfaces -Lifecycle hooks -custom pipes -filtering lists

--- APM-Start/readme.txt
+++ APM-Start/readme.txt
@@ -8,4 +8,14 @@ property binding: [img]='value'
 event binding: (click)="toggleImage"
 two way binding: <input [(ngModel)]="listFilter" />
 
+Interface as a type
+export interface IProduct{}
+products: IProduct[] = [];
+
+Interface as a feature set
+export interface DoTiming {
+  count: number;
+  start(index:number):void}
+  }
+export class myTimingComponent implements DoTiming
 
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+
 
 @Component({
   selector: 'pm-products',
-  templateUrl: '/product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
   pageTitle: string = 'Product list';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = '';
-  products: any[] = [
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value)
+    console.log(`listFilter setter {value}`)
+  }
+
+  filteredProducts: IProduct[] = [];
+
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -35,5 +52,16 @@ export class ProductListComponent {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('ProductListComponent');
+    this.filteredProducts = this.performFilter('');
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy));
   }
 }
